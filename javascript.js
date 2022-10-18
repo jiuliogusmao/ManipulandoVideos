33b6634f3848a22ccbcae628ec03525406c94fb7
Jiulio Gusmão <jiuliogusmao@gmail.com>
2022-10-18 09:52:55 -0300

    Atualização da tag <video> para a tag <iframe> para reproduzir.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
+
 function controlarVideo() {
- 
+	
 	var video = document.getElementById("meuVideo");
 	var btn = document.getElementById("controle");
  
   	if (video.paused) {
    	 	video.play();
-  	  	btn.innerHTML = "Pausar";
+  	  	btn.innerHTML = "Play";
   	} else {
   	  	video.pause();
   	  	btn.innerHTML = "Continuar";
